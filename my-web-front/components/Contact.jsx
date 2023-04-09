4f9c079a0e2985cf0edaaa4c2915db4a6a674966
Sheng <83986522+hoooowsh@users.noreply.github.com>
2023-04-09 19:35:54 -0400

    contact me page finish

--- my-web-front/components/Contact.jsx
+++ my-web-front/components/Contact.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import contactus from "../public/assets/contact-us.jpg";
+import contactus from "../public/assets/contact.jpg";
 import {
   FaBars,
   FaTimes,
@@ -19,35 +19,39 @@ const Contact = () => {
   const [phone, setPhone] = useState("");
 
   return (
-    <div id="contact" className="w-full">
+    <div id="contact" className="w-full bg-slate-700">
       <div className="max-w-screen-xl mx-auto px-8 py-16 text-center md:text-left">
         <h2 className="text-5xl md:text-7xl tracking-wider uppercase text-blue-500 font-bold">
           contact
         </h2>
-        <div className="flex flex-col md:flex-row gap-8 shadow-xl shadow-blue-300 bg-white mt-4">
-          <div className="w-full md:w-1/2 h-full rounded-xl p-4">
+        <div className="flex flex-col md:flex-row gap-8 bg-slate-600 mt-4">
+          <div className="w-full md:w-1/2 h-full rounded-xl p-4 pt-20">
             <Image
               src={contactus}
               alt="man with laptop"
               className="rounded-xl"
             />
-            <p className="pt-2 pb-8">
+            {/* <p className="pt-2 pb-8">
               i am a goooooooooooooooooooooooooooood
               mannnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
-            </p>
-            <div className="grid grid-cols-2 mx-auto w-4/5 gap-10">
-              <div className="flex items-center justify-center rounded-full shadow-md shadow-blue-500 hover:scale-105 duration-200 p-3 cursor-pointer">
-                <FaLinkedin size={25} />
-              </div>
-              <div className="flex items-center justify-center rounded-full shadow-md shadow-blue-500 hover:scale-105 duration-200 p-3 cursor-pointer">
+            </p> */}
+            <div className="grid grid-cols-2 mx-auto w-4/5 gap-10 pt-8">
+              <a href="https://www.linkedin.com/in/sheng-hao-5a91511a2/">
+                <div className="flex items-center justify-center rounded-full hover:scale-105 duration-200 p-3 cursor-pointer bg-slate-500">
+                  <FaLinkedin size={25} />
+                </div>
+              </a>
+              {/* <div className="flex items-center justify-center rounded-full hover:scale-105 duration-200 p-3 cursor-pointer bg-slate-500">
                 <FaTwitter size={25} />
               </div>
-              <div className="flex items-center justify-center rounded-full shadow-md shadow-blue-500 hover:scale-105 duration-200 p-3 cursor-pointer">
+              <div className="flex items-center justify-center rounded-full hover:scale-105 duration-200 p-3 cursor-pointer bg-slate-500">
                 <FaFacebook size={25} />
-              </div>
-              <div className="flex items-center justify-center rounded-full shadow-md shadow-blue-500 hover:scale-105 duration-200 p-3 cursor-pointer">
-                <FaGithub size={25} />
-              </div>
+              </div> */}
+              <a href="https://github.com/hoooowsh">
+                <div className="flex items-center justify-center rounded-full hover:scale-105 duration-200 p-3 cursor-pointer bg-slate-500">
+                  <FaGithub size={25} />
+                </div>
+              </a>
             </div>
           </div>
 
@@ -56,7 +60,7 @@ const Contact = () => {
               <form action="">
                 <div className="grid md:grid-cols-2 gap-4 w-full py-2">
                   <div className="flex flex-col">
-                    <label className="capitalize text-sm py-2 text-slate-600 font-light">
+                    <label className="capitalize text-sm py-2 text-white font-light">
                       name
                     </label>
                     <input
@@ -70,7 +74,7 @@ const Contact = () => {
                     />
                   </div>
                   <div className="flex flex-col">
-                    <label className="capitalize text-sm py-2 text-slate-600 font-light">
+                    <label className="capitalize text-sm py-2 text-white font-light">
                       phone
                     </label>
                     <input
@@ -84,7 +88,7 @@ const Contact = () => {
                     />
                   </div>
                   <div className="flex flex-col col-span-2">
-                    <label className="capitalize text-sm py-2 text-slate-600 font-light">
+                    <label className="capitalize text-sm py-2 text-white font-light">
                       email
                     </label>
                     <input
@@ -98,7 +102,7 @@ const Contact = () => {
                     />
                   </div>
                   <div className="flex flex-col col-span-2">
-                    <label className="capitalize text-sm py-2 text-slate-600 font-light">
+                    <label className="capitalize text-sm py-2 text-white font-light">
                       message
                     </label>
                     <textarea
